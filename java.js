2059494f0e8907fd4e385f30b92bdcad30347e91
rosahoffer <rosahoffer@live.nl>
2022-12-02 12:32:23 +0100

    nieuwe aanpassingen

---
+++ java.js
@@ -0,0 +1,7 @@
+function openForm() {
+    document.getElementById("stekje-aanmelden").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("stekje-aanmelden").style.display = "none";
+  }
